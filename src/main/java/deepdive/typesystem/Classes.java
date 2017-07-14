ee72926264ca95a183fbc56f4719a76c6f0a0b46
Christian Iwanzik <c.iwanzik@tarent.de>
2017-07-14 14:33:23 +0200

    Example of classes

--- src/main/java/deepdive/typesystem/Classes.java
+++ src/main/java/deepdive/typesystem/Classes.java
@@ -4,12 +4,21 @@ import java.util.ArrayList;
 
 public class Classes {
 
+    // tag::classes[]
     Object o = new Object();
 
     public Classes() {
-        o = "Hallo Welt";   // <-- String is not a primitive!
-        o = new ArrayList();
-        o = true;
-        o = 1;
+        o = "Hallo Welt";     // String is not a primitive!
+        o = new ArrayList();  // Plain object
+        o = true;             // Autoboxing of boolean
+        o = 1;                // Autoboxing of int
+
+        System.out.println(o.toString());
+        System.out.println(o.equals(new Object()));
+
+
+        "Hallo Welt".toString();
+        System.out.println("Hallo" instanceof Object);
     }
+    // end::classes[]
 }
