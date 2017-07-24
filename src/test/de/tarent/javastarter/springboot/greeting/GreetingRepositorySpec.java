2cc81f537c960de9a6f673d61d26d9969016f533
Christian Iwanzik <c.iwanzik@tarent.de>
2017-07-24 14:12:46 +0200

    Adding repo tests... Bad bad Christian. First implemented, then tests. :-(

---
+++ src/test/de/tarent/javastarter/springboot/greeting/GreetingRepositorySpec.java
@@ -0,0 +1,49 @@
+package de.tarent.javastarter.springboot.greeting;
+
+import de.tarent.javastarter.springboot.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class GreetingRepositorySpec {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    GreetingRepository repository;
+
+    @Test
+    public void aRepoCanListAllBasicGreetings() {
+        //expect
+        assertEquals(2, repository.getAll().size());
+    }
+
+    @Test
+    public void aRepoCanListASingleGreeting() {
+        // when
+        Greeting greeting = repository.get(1);
+
+        //then
+        assertEquals(1, greeting.getId());
+        assertEquals("Hallo Welt", greeting.getGreeting());
+    }
+
+    @Test
+    public void aRepoCanListAnotherGreeting() {
+        // when
+        Greeting greeting = repository.get(2);
+
+        //then
+        assertEquals(2, greeting.getId());
+        assertEquals("Hello World", greeting.getGreeting());
+    }
+
+}
