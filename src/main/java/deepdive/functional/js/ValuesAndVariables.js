e548b8d55946cc141c8b6288d5061939d0efa02e
Christian Iwanzik <c.iwanzik@tarent.de>
2017-12-28 12:39:14 +0100

    Adding first text for functional programming.

---
+++ src/main/java/deepdive/functional/js/ValuesAndVariables.js
@@ -0,0 +1,17 @@
+
+//tag::imperative[]
+
+// variables as description for a `memory bucket`.
+var variable = "Hallo Welt";
+variable = "Hallo Variable";
+
+//end::imperative[]
+
+
+
+//tag::functional[]
+
+// Values (here `5`) only have names.
+const name = 5;
+
+//end::functional[]
