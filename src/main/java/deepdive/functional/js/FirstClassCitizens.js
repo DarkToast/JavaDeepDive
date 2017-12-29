5e0e33499f55dc4658a0ff48fe96b2dba57ccbc2
Christian Iwanzik <c.iwanzik@tarent.de>
2017-12-29 12:28:48 +0100

    Adding first class citizens.

---
+++ src/main/java/deepdive/functional/js/FirstClassCitizens.js
@@ -0,0 +1,33 @@
+
+// tag::classic[]
+function add(x, y) {
+    return x + y
+}
+
+add(1, 2); // 3
+// end::classic[]
+
+
+
+// tag::f_as_val[]
+const fAdd = function add(x, y) {
+    return x + y
+};
+
+const fAdd2 = (x, y) => {
+    return x + y;
+};
+
+fAdd(1, 2);
+fAdd2(1, 2);
+// end::f_as_val[]
+
+
+
+// tag::f_as_param[]
+const usingAFunction = (addFunction) => {
+    return addFunction(4, 8);
+};
+
+usingAFunction(add); // 12;
+// end::f_as_param[]
