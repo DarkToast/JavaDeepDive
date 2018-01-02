f61325b0b3ce36d3a1dbbbed2d11b93d36c1ba1d
Christian Iwanzik <c.iwanzik@tarent.de>
2018-01-02 15:03:17 +0100

    Adding the higher order section.

--- src/main/js/deepdive/functional/HigherOrderFunction.js
+++ src/main/js/deepdive/functional/HigherOrderFunction.js
@@ -1,15 +1,3 @@
-
-//tag::function_builder[]
-const greaterThan = x => {
-    return (y) => x > y
-};
-
-const f = greaterThan(10);
-f(11); // true
-//end::function_builder[]
-
-
-
 //tag::function_parameter[]
 const mapX = (x, xMapper) => {
     return xMapper(x)
@@ -19,7 +7,31 @@ mapX(10, i => i * i);
 //end::function_parameter[]
 
 
-
 //tag::lib_example[]
 [1, 2, 3].map(i => i + 2);
 //end::lib_example[]
+
+
+//tag::higher_order_function[]
+const getEqualFunction = () => {
+  return (x, y) => x === y;
+};
+
+const equalFunction = getEqualFunction();
+equalFunction(1, 2); // false
+//end::higher_order_function[]
+
+
+//tag::function_builder[]
+const greaterThan = x => {
+    return (y) => x > y
+};
+
+const f = greaterThan(10);
+f(9);  // false
+f(11); // true
+
+const g = greaterThan(15);
+g(11); // false
+g(16); // true
+//end::function_builder[]
